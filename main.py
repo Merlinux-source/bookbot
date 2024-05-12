c26afd6cbb48d9eb95a344e54e7b162b283675fa
Merlinux-source <master@merlinux.xyz>
2024-05-12 14:46:08 +0200

    fix report layout

--- main.py
+++ main.py
@@ -43,22 +43,20 @@ def __main__():
         return exit(errno.E2BIG)
     
     book_content = get_book_text(argv[1])
-    print(f"+--------------------------------------------------------+")
-    print(f"|                                                        |")
-    print(f"|            Generating report for {argv[1]}             |")
-    print(f"|                                                        |")
-    print(f"+--------------------------------------------------------+")
-    print(f"| {argv[1]} contains {count_words(book_content)} words.  |")
-    print(f"| {argv[1]} contains {count_chars(book_content)} letters |")
-    print(f"+--------------------------------------------------------+")
-    print(f"|           Reporting character frequency                |")
-    print(f"|                                                        |")
+    print(f"                                                        ")
+    print(f"            Generating report for {argv[1]}             ")
+    print(f"                                                        ")
+    print(f"")
+    print(f" {argv[1]} contains {count_words(book_content)} words.  ")
+    print(f" {argv[1]} contains {count_chars(book_content)} letters ")
+    print(f"")
+    print(f"           Reporting character frequency                ")
+    print(f"                                                        ")
 
     char_frequency = report_char_frequency(book_content)
     for char_key in char_frequency:
-        print(f"| {char_key} was found {char_frequency[char_key]} times. |")
+        print(f" {char_key} was found {char_frequency[char_key]} times. ")
 
-    print(f"+--------------------------------------------------------+")
 
 if __name__ == "__main__":
     __main__()
