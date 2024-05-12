6f3bfec5194859d5f86ffc5ea6f0dcd4a4046da6
Merlinux-source <master@merlinux.xyz>
2024-05-12 14:13:53 +0200

    add usage information and argument handeling

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from sys import argv
+import errno
+
+argc = len (argv)
 def __main__():
     print("Hello world!")
+    if (argc == 1):
+        print(f"Usage: ./{argv[0]} [book path]")
+        return exit(errno.EINVAL)
+    if (argc > 3):
+        print(f"Usage: ./{argv[0]} [book path]")
+        print("E2BIG error 7")
+        return exit(errno.E2BIG)
 
 if __name__ == "__main__":
     __main__()
